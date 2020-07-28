d47b36f5dc3f645c938089d4080d34060cc6d896
flatychen <flatychen@126.com>
2020-07-28 18:10:58 +0800

    add codility

--- src/main/java/flaty/codility/binaryGap/Solution.java
+++ src/main/java/flaty/codility/binaryGap/Solution.java
@@ -1,16 +1,29 @@
 package flaty.codility.binaryGap;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 /**
  * @author flaty
  * @date 2020-7-28
  */
 public class Solution {
 
-    public int solution(int A) {
+    public int solution(int b) {
+        int location[] = new int[128];
+        int l = 0;
+        for (int i = 0; b > 0; i++) {
+            if ((b & 1) != 0) {
+                location[l++] = i + 1;
+            }
+            b = b >>> 1;
+        }
+        int max = 0;
+        for (int i = 1; i < l; i++) {
+            max = Math.max(max,location[i]-location[i-1]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        new Solution().solution(22);
     }
 
 }
