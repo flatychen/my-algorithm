7d1b944c12b2ad3867db130969bc2a8a1f8d0308
flatychen <flatychen@126.com>
2020-07-30 18:01:28 +0800

    add codility

---
+++ src/main/java/flaty/codility/missingInteger/Solution2.java
@@ -0,0 +1,27 @@
+package flaty.codility.missingInteger;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author flaty
+ * @date 2020-7-28
+ */
+public class Solution2 {
+
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            set.add(A[i]);
+        }
+        for (int i = 0; i < A.length; i++) {
+            if (!set.contains(i + 1)) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+}
+
